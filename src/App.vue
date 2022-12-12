d50f4b9bb8c6d2b895188a128eedafcde86b113a
ippoRulez <ippolitodonato@gmail.com>
2022-12-12 17:48:15 +0100

    initial commit + filter starting logic

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+	<div class="main-container text-bg-dark">
+		<h1 class="text-center">Hello Net</h1>
+
+		<TheHeader />
+	</div>
+</template>
+
+<script>
+import axios from 'axios';
+import TheHeader from './components/TheHeader.vue';
+import {store} from './store';
+
+export default {
+	components: {TheHeader},
+
+	methods: {
+		fetchMovies() {
+			axios.get('https://api.themoviedb.org/3/search/', {
+				params: {
+					api_key: '4be321c822eae29fbba299f60ddac453',
+					query: store.searchText,
+				},
+			});
+		},
+	},
+};
+</script>
+
+<style scoped>
+.main-container {
+	height: 100vh;
+}
+</style>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<template>
+	<div class="searchBar input-group">
+		<input
+			v-model="store.searchText"
+			@click="performSearch"
+			class="form-control"
+			type="text"
+			placeholder="Search a film" />
+		<button class="btn btn-success">Go</button>
+	</div>
+</template>
+
+<script>
+import {store} from '../store';
+export default {
+	data() {
+		return {
+			store,
+		};
+	},
+};
+</script>
+
+<style lang="scss"></style>
